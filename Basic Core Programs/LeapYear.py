09590ee5e1d2a0850fa9737696dade76c0b9aae1
unknown <mfjkd7795@gmail.com>
2021-09-17 00:42:57 -0700

    [Mohammad] Refactor: Program to check given year is leap year or not and used Regex to ensure the year should have 4 digit number

--- Basic Core Programs/LeapYear.py
+++ Basic Core Programs/LeapYear.py
@@ -10,14 +10,15 @@ import re
 
 Year = int(input("Enter the year to check it is leap year or not : "))
 
-if re.match("^[0-9]{4}"):
+if re.match("^[0-9]{4,}$",str(Year)):
     if (Year%4) == 0:
         if ( Year % 100 ) == 0:
             if ( Year % 400 ) == 0:
-                print("Entred year {0} is leap a year".format(Year))
+                print(f"Entred year {Year} is leap a year")
+                # print("Entred year {0} is leap a year".format(Year))
             else:
-                print("Entred year {0} is not leap year".format(Year))
+                print(f"Entred year {Year} is not leap year")
         else:
-            print("Entred year {0} is a leap year".format(Year))
+            print(f"Entred year {Year} is a leap year")
     else:
-        print("Entred year {0} is not leap year".format(Year))
+        print(f"Entred year {Year} is not leap year")
