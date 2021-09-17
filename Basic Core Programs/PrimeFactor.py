e73ae9911bece40c2a3ca9a0fc77ba20bf9b86c4
unknown <mfjkd7795@gmail.com>
2021-09-17 00:54:48 -0700

    [Mohammad] Refactor: Program to print all the prime factors of the given number

--- Basic Core Programs/PrimeFactor.py
+++ Basic Core Programs/PrimeFactor.py
@@ -5,11 +5,23 @@
 * @Last Modified time: 2021-09-17 00:42
 * @Title: To print the prime factors of the given number
 '''
+import math as m
+
 Num = int(input("Enter the number to find it's prime factors : "))
 
-print("Prime factors of the given number {0} are".format(Num))
+print(f"Prime factors of the given number {Num} are")
 
-for i in range(1,Num+1):
-    temp = Num % i
-    if temp == 0:
+# Even number divisible
+while (Num % 2 == 0):
+    print(2)
+    Num = Num/2
+
+# number become odd
+for i in range(3, int(m.sqrt(Num))+1,2):
+
+    while (Num % i == 0):
         print(i)
+        Num = Num/ i
+
+if Num > 2:
+    print(Num)
