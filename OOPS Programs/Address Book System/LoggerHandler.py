04cc182d183f7bdcc89bae96cfee61c0750fd0c6
unknown <mfjkd7795@gmail.com>
2021-09-21 05:30:06 -0700

    [Mohammad] Add: Log handler code with info and error level

--- OOPS Programs/Address Book System/LoggerHandler.py
+++ OOPS Programs/Address Book System/LoggerHandler.py
@@ -3,6 +3,7 @@ import logging
 logger = logging
 
 # Basic configuration and required format of the file
+
 logger.basicConfig(filename="F:\.metadata\Python\OOPS Programs\Address Book System\ addressbook.log",
                     level=logging.INFO, format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
 
